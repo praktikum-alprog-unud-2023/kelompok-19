f8f8c4b2c3d361848fc8fc00cd3695ff4a5e34bc
deaaalma <hopegekdea@gmail.com>
2023-10-16 16:23:52 +0800

    Update Volume.

--- modul-1/soal-3/soal-3.c
+++ modul-1/soal-3/soal-3.c
@@ -1,126 +1,78 @@
-#include<stdio.h>
-#include<math.h>
-
-// Struktur Penyimpanan
-struct Tabung
+#include <stdio.h>
+#include <math.h>
+#define M_PI 3.14159265358979323846
+struct Hasil
 {
-double JariJari;
-double tinggi;
+    double volume;
+    double luaspermukaan;
 };
 
-// Fungsi menghitung Volume Tabung
-double VolumeTabung (struct Tabung t)
+int validasi_angka(int minimum, int maximum) // Hanya sebuah validasi angka biasa.
 {
-return M_PI * pow(t.JariJari, 2)* t.tinggi;
+    char buff[255], invalid;
+    int valid;
+    scanf("%[^\n]", buff);
+    getchar();
+    if (sscanf(buff, "%d%c", &valid, invalid) == 1 && valid >= minimum && valid <= maximum)
+        return valid;
+    else
+    {
+        printf("Input yang kamu masukkan salah!\nHarap masukkan kembali!\n>> ");
+        validasi_angka(minimum, maximum);
+    }
 }
 
-//Fungsi menghitung Luas Permukaan Tabung
-double LuasPermukaanTabung (struct Tabung t)
+double validasi_tabung(double minimum) // Hanya sebuah validasi angka biasa.
 {
-double luasAlas = 2 * M_PI * pow(t.JariJari, 2);
-double luasSelimut = 2* M_PI * t.JariJari * t.tinggi;
-return luasAlas + luasSelimut ;
+    char buff[255], invalid;
+    double valid;
+    scanf("%[^\n]", buff);
+    getchar();
+    if (sscanf(buff, "%lf%c", &valid, invalid) == 1 && valid >= minimum)
+        return valid;
+    else
+    {
+        printf("Input yang kamu masukkan salah!\nHarap masukkan kembali!\n>> ");
+        validasi_tabung(minimum);
+    }
+
 }
 
-struct bola 
+struct Hasil tabung(double jarijari, double tinggi)
 {
-double JariJari;
-};
+    struct Hasil hasil;
 
-double VolumeBola(struct bola b) 
-{
-return (4 * M_PI * pow(b.JariJari, 3)) / 3;
-}
-
-double LuasPermukaanBola(struct bola b) 
-{
-return 4 * M_PI * pow(b.JariJari, 2);
-}
-
-struct kerucut 
-{
-float radius;
-float tinggi;
-float sisiMiring;
-};
-
-float hitungVolume(struct kerucut kerucut) {
-return (1 * M_PI * pow(kerucut.radius, 2) * kerucut.tinggi) / 3;
-}
-
-float hitungLuasPermukaan(struct kerucut kerucut) {
-return M_PI * kerucut.radius * (kerucut.radius + kerucut.sisiMiring);
+    hasil.luaspermukaan = 2 * M_PI * jarijari * (jarijari + tinggi);
+    hasil.volume = 2 * M_PI * jarijari * (jarijari + tinggi);
+    return hasil;
 }
 
 int main()
 {
-int Program;
-struct Tabung Tabung;
-struct bola bola;
-struct kerucut kerucut;
-// Input Operator
-printf ("\n");
-printf ("\t\t\t\tProgram Menghitung Volume dan luas permukaan bangun ruang\n");
-printf ("Pilih Program (1-5) : \n");
-printf ("\n 1. Tabung\n 2. Bola\n 3. limas Segiempat\n 4. Prisma Segitiga\n 5. Kerucut\n");
-printf("\n\nPilihan mu: ");
-scanf("%d", &Program);
+    int program;
+    struct Hasil hasil;
+    double jarijari, tinggi;
 
-switch (Program) // Memilih operator dalam list
-{
-case 1 : //Tabung //Punya Linda
-printf("Masukan Jari_Jari Tabung: ");
-scanf("%lf", &Tabung.JariJari);
-printf("Masukan Tinggi Tabung: ");
-scanf("%lf", &Tabung.tinggi);
+    // Input Operator
+    printf("\n");
+    printf("\t\t\t\tProgram Menghitung Volume dan luas permukaan bangun ruang\n");
+    printf("Pilih Program (1-5) : \n");
+    printf("\n 1. Tabung\n 2. Bola\n 3. limas Segiempat\n 4. Prisma Segitiga\n 5. Kerucut\n");
+    printf("\n\nPilihan mu: ");
+    program = validasi_angka(1,5);
+    switch (program) //  Memilih operator dalam list
+    {
+    case 1: // Tabung //Punya Linda
 
-//menghitung volume dan luas permukaan tabung
-double volume = VolumeTabung(Tabung);
-double luasPermukaan = LuasPermukaanTabung(Tabung);
-
-//menampilkan hasil
-printf ("Volume Tabung : %lf\n" , volume);
-printf ("Luas Permukaan Tabung : %lf\n", luasPermukaan);
-break;
-
-case 2 : //Bola //Punya Dewaayu
-printf("Masukkan nilai jari-jari: ");
-scanf("%lf", &bola.JariJari);
-
-double volumebola = VolumeBola(bola);
-double luas = LuasPermukaanBola(bola);
-
-printf("Volume bola= %.2lf\n", volumebola);
-printf("Luas permukaan bola= %.2lf\n", luas);
-break;
-
-case 3 : //limasSegiempat //Punya Alit
-
-break;
-
-case 4 : //prismaSegitiga //Punya Alit
-
-break;
-
-case 5 : //Kerucut //Punya Dewaayu
-printf("Masukkan nilai jari-jari: ");
-scanf("%f", &kerucut.radius);
-printf("Masukkan nilai tinggi: ");
-scanf("%f", &kerucut.tinggi);
-printf("Masukkan nilai sisi miring: ");
-scanf("%f", &kerucut.sisiMiring);
-
-float volumeKerucut = hitungVolume(kerucut);
-printf("Volume kerucut= %.2f\n", volumeKerucut);
-
-float luasKerucut = hitungLuasPermukaan(kerucut);
-printf("Luas permukaan kerucut= %.2f\n", volumeKerucut);
-
-break;
-
-default : //Validasi
-printf ("Pilihan tidak ada dalam list, silahkan restart");
-break;
-}
-return 0;
+        printf("Masukan Jari_Jari Tabung: ");
+        jarijari= validasi_tabung(0);
+        printf("Masukan Tinggi Tabung: ");
+        tinggi = validasi_tabung(0);
+        hasil = tabung(jarijari, tinggi);
+        // menampilkan hasil
+        printf("Volume Tabung : %lf\n", hasil.volume);
+        printf("Luas Permukaan Tabung : %lf\n", hasil.luaspermukaan);
+        break;
+    }
+    return 0;
 }
