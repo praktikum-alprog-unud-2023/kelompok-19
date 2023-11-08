ff98b0a83bfe34866e9e7a34b5b67c7e603c387c
gusdezikye <97604247+gusdezikye@users.noreply.github.com>
2023-11-08 13:58:52 +0700

    Update soal-5.c

--- modul-2/soal-5/soal-5.c
+++ modul-2/soal-5/soal-5.c
@@ -2,49 +2,61 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-int tower_of_hanoi_recursive(int n, char source, char auxiliary, char target) {
+int towerOfHanoiRecursive(int n, char source, char auxiliary, char target) {
     if (n == 1) {
-        printf("Pindahkan cakram 1 dari %c ke %c\n", source, target);
+        printf("Move disk 1 from %c to %c\n", source, target);
         return 1;
     } else {
-        int steps1 = tower_of_hanoi_recursive(n - 1, source, target, auxiliary);
-        printf("Pindahkan cakram %d dari %c ke %c\n", n, source, target);
-        int steps2 = tower_of_hanoi_recursive(n - 1, auxiliary, source, target);
+        int steps1 = towerOfHanoiRecursive(n - 1, source, target, auxiliary);
+        printf("Move disk %d from %c to %c\n", n, source, target);
+        int steps2 = towerOfHanoiRecursive(n - 1, auxiliary, source, target);
         return steps1 + 1 + steps2;
     }
 }
 
-int tower_of_hanoi_iterative(int n) {
-    int total_steps = (1 << n) - 1;
-    return total_steps;
+int towerOfHanoiIterative(int n) {
+    int totalSteps = (1 << n) - 1;
+    return totalSteps;
+}
+
+int validateInteger(int min, int max) {
+    char buff[255], *invalid;
+    int valid;
+    do {
+        scanf("%[^\n]", buff);
+        getchar();
+        if (sscanf(buff, "%d%c", &valid, invalid) == 1 && valid >= min && valid <= max)
+            return valid;
+        else
+            printf("Wrong input, try again!\n>> ");
+    } while (1);
 }
 
 int main() {
     char input[100];
     int n;
-    while (1) {
-        printf("Masukkan jumlah cakram: ");
-        fgets(input, sizeof(input), stdin);
-        if (sscanf(input, "%d", &n) == 1 && n > 0) {
-            int total_steps_recursive = tower_of_hanoi_recursive(n, 'A', 'B', 'C');
-            printf("Jumlah langkah minimum (metode rekursif): %d\n", total_steps_recursive);
+    int choice = 1;
 
-            int total_steps_iterative = tower_of_hanoi_iterative(n);
-            printf("Jumlah langkah minimum (metode iteratif): %d\n", total_steps_iterative);
-        } else {
-            printf("Masukkan angka yang valid.\n");
+    while (choice == 1) {
+        printf("Enter the number of disks: ");
+        fgets(input, sizeof(input), stdin);
+
+        // Validate input as a number
+        if (sscanf(input, "%d", &n) != 1 || n <= 0) {
+            printf("Please enter a valid number.\n");
             continue;
         }
 
-        char choice;
-        printf("Apakah Anda ingin menghitung lagi? (y/n): ");
-        fgets(input, sizeof(input), stdin);
-        sscanf(input, "%c", &choice);
-        if (tolower(choice) != 'y') {
-            break;
-        }
+        int totalStepsRecursive = towerOfHanoiRecursive(n, 'A', 'B', 'C');
+        printf("Minimum number of steps (recursive method): %d\n", totalStepsRecursive);
+
+        int totalStepsIterative = towerOfHanoiIterative(n);
+        printf("Minimum number of steps (iterative method): %d\n", totalStepsIterative);
+
+        printf("Do you want to calculate again? (0/1): ");
+
+        choice = validateInteger(0, 1);
     }
 
     return 0;
 }
-
