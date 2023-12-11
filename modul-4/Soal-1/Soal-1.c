42a05ed400bafa6b5b271502bb620f3d69b63d65
NartofrenlynAritonang <147513537+NartofrenlynAritonang@users.noreply.github.com>
2023-12-11 15:01:57 +0700

    Soal-1.c

--- modul-4/Soal-1/Soal-1.c
+++ modul-4/Soal-1/Soal-1.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+// Define struct for salary data
+struct Salary {
+    float hourlyWage;
+    float overtimeHourlyWage;
+};
+
+int main() {
+    // Initialize salary data within the struct
+    struct Salary wageData;
+    wageData.hourlyWage = 10625;
+    wageData.overtimeHourlyWage = 8 * wageData.hourlyWage;
+
+    int hoursWorked;
+    float totalWage = 0.0;
+
+    printf("Enter the number of hours worked: ");
+    scanf("%d", &hoursWorked);
+
+    // Calculate wage based on hours worked
+    if (hoursWorked <= 8) {
+        totalWage = hoursWorked * wageData.hourlyWage;
+    } else {
+        totalWage = 8 * wageData.hourlyWage + (hoursWorked - 8) * wageData.overtimeHourlyWage;
+    }
+
+    // Display the daily wage calculation details
+    printf("Daily Wage Details:\n");
+    printf("Hours Worked: %d hours\n", hoursWorked);
+    printf("Hourly Wage: Rp%.2f\n", wageData.hourlyWage);
+    printf("Overtime Hourly Wage: Rp%.2f\n", wageData.overtimeHourlyWage);
+    printf("Total Daily Wage: Rp%.2f\n", totalWage);
+
+    return 0;
+}
 
